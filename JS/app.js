97ec90533786a0cb99778c7e6cc01c8006aa528a
Ever Cardozo <everecardozo@outlook.com>
2025-02-15 10:26:56 -0300

    comienzo de proyecto

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+console.log('Hola soy el script');
+
+//C-R-U-D 
+//CREATE
+
+function createPerson (e){
+    e.preventDefault();
+    const nombre = document.getElementById ('nombre').ariaValueMax.trim
+}
+
+//READ-RETRIEVE
+//UPDATE
+//DELETE
